e6e3b6150e6bffac416a9b3efb9e17bd78db26b5
jonashw <2jonwilson@gmail.com>
2012-12-14 21:25:14 -0800

    Removed deploy script, coldfusion code

--- deploy.cmd
+++
@@ -1,5 +0,0 @@
-cp jQuery.dynamicTable.min.js v:\websites\universal\javascript\jQuery.dynamicTable.min.js
-cp jQuery.dynamicTable.min.js w:\websites\universal\javascript\jQuery.dynamicTable.min.js
-
-cp jQuery.dynamicTable.bootstrap.min.js v:\websites\universal\javascript\jQuery.dynamicTable.bootstrap.min.js
-cp jQuery.dynamicTable.bootstrap.min.js w:\websites\universal\javascript\jQuery.dynamicTable.bootstrap.min.js
